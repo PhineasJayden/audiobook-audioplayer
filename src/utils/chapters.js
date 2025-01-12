64e8f64a8186cda51383b8728344a577c2ea0d37
Phineas Jayden <phineas.jayden@outlook.de>
2025-01-12 06:34:39 +0100

    added mp3 and images

--- src/utils/chapters.js
+++ src/utils/chapters.js
@@ -1,26 +1,136 @@
-import k01_audio from '../assets/audio/k01_audio.mp3';
-import k02_audio from '../assets/audio/k02_audio.mp3';
+import k01_audio from '../assets/audio/DieKleinenHolzdiebe_01.mp3';
+import k02_audio from '../assets/audio/DieKleinenHolzdiebe_02.mp3';
+import k03_audio from '../assets/audio/DieKleinenHolzdiebe_03.mp3';
+import k04_audio from '../assets/audio/DieKleinenHolzdiebe_04.mp3';
+import k05_audio from '../assets/audio/DieKleinenHolzdiebe_05.mp3';
+import k06_audio from '../assets/audio/DieKleinenHolzdiebe_06.mp3';
+import k07_audio from '../assets/audio/DieKleinenHolzdiebe_07.mp3';
+import k08_audio from '../assets/audio/DieKleinenHolzdiebe_08.mp3';
+import k09_audio from '../assets/audio/DieKleinenHolzdiebe_09.mp3';
+import k10_audio from '../assets/audio/DieKleinenHolzdiebe_10.mp3';
+import k11_audio from '../assets/audio/DieKleinenHolzdiebe_11.mp3';
+import k12_audio from '../assets/audio/DieKleinenHolzdiebe_12.mp3';
 
-import K_01_prev from '../assets/images/K_01_prev.png';
-import K_02_prev from '../assets/images/K_02_prev.png';
+import K_01_prev from '../assets/images/Kapitel_01.jpg';
+import K_02_prev from '../assets/images/Kapitel_02.jpg';
+import K_03_prev from '../assets/images/Kapitel_03.jpg';
+import K_04_prev from '../assets/images/Kapitel_04.jpg';
+import K_05_prev from '../assets/images/Kapitel_05.jpg';
+import K_06_prev from '../assets/images/Kapitel_06.jpg';
+import K_07_prev from '../assets/images/Kapitel_07.jpg';
+import K_08_prev from '../assets/images/Kapitel_08.jpg';
+import K_09_prev from '../assets/images/Kapitel_09.jpg';
+import K_10_prev from '../assets/images/Kapitel_11.jpg';
+import K_11_prev from '../assets/images/Kapitel_11.jpg';
+import K_12_prev from '../assets/images/Kapitel_12.jpg';
 
 export const chapters = [
   {
-    id: 'spk363636',
+    id: 'holzdiebe1',
     chapter: 1,
-    title: 'Todgeweiht',
+    title: 'Kapitel 1',
     src: k01_audio,
-    time: '03:50',
+    time: '08:12',
     imgPreview: K_01_prev,
-    alt: 'Spooky sitzt zusammengekauert auf seinem Bett',
+    alt: '',
   },
   {
-    id: 'spkBF3030',
+    id: 'holzdieb2',
     chapter: 2,
-    title: 'Schwellenwert',
+    title: 'Kapitel 2',
     src: k02_audio,
-    time: '03:48',
+    time: '13:00',
     imgPreview: K_02_prev,
-    alt: 'Spooky schwebt mit angezogenen Beinen ängstlich durch einen Gang. Aus dem Boden ragen Schattenhände mit klauen',
+    alt: '',
+  },
+  {
+    id: 'holzdiebe3',
+    chapter: 3,
+    title: 'Kapitel 3',
+    src: k03_audio,
+    time: '29:51',
+    imgPreview: K_03_prev,
+    alt: '',
+  },
+  {
+    id: 'holzdiebe4',
+    chapter: 4,
+    title: 'Kapitel 4',
+    src: k04_audio,
+    time: '18:02',
+    imgPreview: K_04_prev,
+    alt: '',
+  },
+  {
+    id: 'holzdiebe5',
+    chapter: 5,
+    title: 'Kapitel 5',
+    src: k05_audio,
+    time: '09:42',
+    imgPreview: K_05_prev,
+    alt: '',
+  },
+  {
+    id: 'holzdiebe6',
+    chapter: 6,
+    title: 'Kapitel 6',
+    src: k06_audio,
+    time: '11:30',
+    imgPreview: K_06_prev,
+    alt: '',
+  },
+  {
+    id: 'holzdiebe7',
+    chapter: 7,
+    title: 'Kapitel 7',
+    src: k07_audio,
+    time: '33:58',
+    imgPreview: K_07_prev,
+    alt: '',
+  },
+  {
+    id: 'holzdiebe8',
+    chapter: 8,
+    title: 'Kapitel 8',
+    src: k08_audio,
+    time: '21:10',
+    imgPreview: K_08_prev,
+    alt: '',
+  },
+  {
+    id: 'holzdiebe9',
+    chapter: 9,
+    title: 'Kapitel 9',
+    src: k09_audio,
+    time: '1:00:09',
+    imgPreview: K_09_prev,
+    alt: '',
+  },
+  {
+    id: 'holzdiebe10',
+    chapter: 10,
+    title: 'Kapitel 10',
+    src: k10_audio,
+    time: '29:49',
+    imgPreview: K_10_prev,
+    alt: '',
+  },
+  {
+    id: 'holzdiebe11',
+    chapter: 11,
+    title: 'Kapitel 11',
+    src: k11_audio,
+    time: '24:15',
+    imgPreview: K_11_prev,
+    alt: '',
+  },
+  {
+    id: 'holzdiebe12',
+    chapter: 12,
+    title: 'Kapitel 12',
+    src: k12_audio,
+    time: '53:54',
+    imgPreview: K_12_prev,
+    alt: '',
   },
 ];
